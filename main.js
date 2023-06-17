d9308d6e95d2328a28f873a21bdec1df2db948d2
tayseeressa <128232016+tayseeressa@users.noreply.github.com>
2023-06-17 05:57:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+name_of_the_student_array=[];
+function submit()
+{
+    var name_1= document.getElementById("name_of_the_student_1").value;
+
+    var name_2= document.getElementById("name_of_the_student_2").value;
+    var name_3= document.getElementById("name_of_the_student_3").value;
+    var name_4= document.getElementById("name_of_the_student_4").value;
+    var name_5= document.getElementById("name_of_the_student_5").value;
+
+name_of_the_student_array.push(name_1);
+name_of_the_student_array.push(name_2);
+name_of_the_student_array.push(name_3);
+name_of_the_student_array.push(name_4);
+name_of_the_student_array.push(name_5);
+
+
+
+
+
+
+
+console.log(name_of_the_student_array);
+
+
+
+document.getElementById("display_name").innerHTML= name_of_the_student_array;
+}
